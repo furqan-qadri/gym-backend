5ff4ac0f9b1ab93a52a6890d910b2592a2430be8
furqan-qadri <furqaanqadri@gmail.com>
2024-06-16 20:45:58 +0800

    add newsletter email

--- server.js
+++ server.js
@@ -11,26 +11,40 @@ const Mailgun = require("mailgun.js");
 const mailgun = new Mailgun(formData);
 const mg = mailgun.client({
   username: "api",
-  key: process.env.MAILGUN_API_KEY || "222",
+  key: process.env.MAILGUN_API_KEY,
 });
 
-mg.messages
-  .create("mail.furqanqadri.com", {
-    from: "Furqan Qadri <contact@mail.furqanqadri.com>",
-    to: ["furqanqadri1@outlook.com"],
-    subject: "for the outlook only? yup baby",
-    text: "Testing some Mailgun awesomeness!",
-    html: "<h1>Testing some Mailgun awesomeness!</h1>",
-  })
-  .then((msg) => console.log(msg)) // logs response data
-  .catch((err) => console.log(err)); // logs any error
+// mg.messages
+//   .create("mail.furqanqadri.com", {
+//     from: "Furqan Qadri <contact@mail.furqanqadri.com>",
+//     to: ["furqanqadri1@outlook.com"],
+//     subject: "for the outlook only? yup baby",
+//     text: "Testing some Mailgun awesomeness!",
+//     html: "<h1>Testing some Mailgun awesomeness!</h1>",
+//   })
+//   .then((msg) => console.log(msg)) // logs response data
+//   .catch((err) => console.log(err)); // logs any error
 
 // Load environment variables
+//twillio whatsapp
+// const accountSid = "";
+// const authToken = "";
+// const client = require("twilio")(accountSid, authToken);
+
+// client.messages
+//   .create({
+//     body: "Your appointment is coming up on July 21 at 3PM yoyo is that alright wohoooooo????",
+//     from: "whatsapp:+14155238886",
+//     to: "whatsapp:+601129990488",
+//   })
+//   .then((message) => console.log(message.sid))
+//   .catch((error) => console.error(error));
 
 // Create an Express application
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors());
 
 // Database configuration
 const dbConfig = {
@@ -57,7 +71,25 @@ app.use("/api/v1/gym/payments", require("./routes/paymentsRoutes"));
 app.use("/api/v1/gym/salaries", require("./routes/salariesRoutes"));
 
 app.get("/", (req, res) => {
-  res.status(200).send("<h1>Furqan and Areeba are a cool couple!! yoyo</h1>");
+  res
+    .status(200)
+    .send("<h1>Furqan and Areeba are a cool couple!! yoyo!d eg</h1>");
+});
+
+//newsletter
+app.post("/subscribe", (req, res) => {
+  const { email } = req.body;
+
+  mg.messages
+    .create("mail.furqanqadri.com", {
+      from: "Furqan Qadri <contact@mail.furqanqadri.com>",
+      to: email,
+      subject: "for the outlook only? yup baby",
+      text: "Testing some Mailgun awesomeness!",
+      html: "<h1>Testing some Mailgun awesomeness!</h1>",
+    })
+    .then((msg) => console.log(msg)) // logs response data
+    .catch((err) => console.log(err)); // logs any error
 });
 
 // Start the server and test the database connection
